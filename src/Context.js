4e9662cd2a043eb599c0ea3a49d003a898632d84
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-04 11:17:32 +0100

    Fetch photos

--- src/Context.js
+++ src/Context.js
@@ -1,10 +1,18 @@
-import React, { createContext, useState } from "react"
+import React, { createContext, useState, useEffect } from "react"
 
 const { Provider } = createContext()
 
 function ContextProvider({children}) {
 
     const [allPhotos, setAllPhotos] = useState([])
+
+    useEffect(() => {
+        fetch("https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json")
+            .then(response => response.json())
+            .then(data => setAllPhotos(data))
+    }, [])
+
+    console.log(allPhotos)
 
     return (
         <Provider value={{allPhotos}}>
