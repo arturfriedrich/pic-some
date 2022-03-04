66de2b09afe0ad55d40b2bd04e7429d25e0e3d93
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-04 12:09:33 +0100

    Track Hover State

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,8 +1,15 @@
-import React from "react"
+import React, { useState } from "react"
 
 export default function Image({img, className}) {
+
+    const [hovered, setHovered] = useState(false)
+
     return (
-        <div className={`${className} image-container`}>
+        <div 
+            className={`${className} image-container`}
+            onMouseEnter= {() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+        >
             <img src={img.url} className="image-grid"/>
         </div>
     )
