4e268b9aa82221d14582e2303c6eb0fe80dbc631
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-04 12:15:00 +0100

    Display Icons Hover

--- src/components/Image.js
+++ src/components/Image.js
@@ -4,6 +4,9 @@ export default function Image({img, className}) {
 
     const [hovered, setHovered] = useState(false)
 
+    const heartIcon = hovered && <i className="ri-heart-line favorite"></i>
+    const cartIcon = hovered && <i className="ri-add-circle-line cart"></i>
+
     return (
         <div 
             className={`${className} image-container`}
@@ -11,6 +14,8 @@ export default function Image({img, className}) {
             onMouseLeave={() => setHovered(false)}
         >
             <img src={img.url} className="image-grid"/>
+            {heartIcon}
+            {cartIcon}
         </div>
     )
 }
