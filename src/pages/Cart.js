5e273503599732229df40cbab2a1fe8ff7a41a55
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-07 22:29:20 +0100

    Update Cart.js

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -4,7 +4,7 @@ import {Context} from "../Context"
 
 import CartItem from "../components/CartItem"
 
-function Cart() {
+export default function Cart() {
 
     const {cartItems} = useContext(Context)
     const cartItemElements = cartItems.map(item => <CartItem key={item.id} item={item} />)
@@ -20,5 +20,3 @@ function Cart() {
         </main>
     )
 }
-
-export default Cart
