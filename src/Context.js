6695b796c2aa84b022d5c360030eec95b3146546
Artúr Friedrich <67378210+arturfriedrich@users.noreply.github.com>
2022-03-07 20:11:04 +0100

    Toggle isFavorited

--- src/Context.js
+++ src/Context.js
@@ -12,10 +12,20 @@ function ContextProvider({children}) {
             .then(data => setAllPhotos(data))
     }, [])
 
-    console.log(allPhotos)
+    function toggleFavorite(id) {
+        const updatedArray = allPhotos.map(photo => {
+            if (photo.id === id) {
+                console.log(photo.id)
+                console.log(!photo.isFavorite)
+                return {...photo, isFavorite: !photo.isFavorite}
+            }
+            return photo
+        })
+        setAllPhotos(updatedArray)
+    }
 
     return (
-        <Context.Provider value={{allPhotos}}>
+        <Context.Provider value={{allPhotos, toggleFavorite}}>
             {children}
         </Context.Provider>
     )
